ea5f19c6a1db655f4f6610f8fa18bac9aa2fbc5a
hikimochi <rhikimochi@gmail.com>
2021-01-25 23:59:14 +0900

    Add voice.

---
+++ voice_emotion.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import requests
+
+url ='https://api.webempath.net/v2/analyzeWav'
+
+apikey = os.environ['API_KEY']
+payload = {'apikey': apikey}
+
+wav = "./voices/watashi.wav"
+data = open(wav, 'rb')
+file = {'wav': data}
+
+res = requests.post(url, params=payload, files=file)
+print(res.json())
